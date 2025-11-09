702d23b98a5baf0ff02c65fe85dfbe92e7b86548
tntksa1 <152206947+tntksa1@users.noreply.github.com>
2025-11-09 12:10:46 +0300

    add jump

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,26 +4,55 @@ public class PlayerMovement : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
-    public float velocity;
-    public float MoveSpeed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private SpriteRenderer sr;
+
+    public float MoveSpeed = 5f;
+    public float JumpForce = 7f;
+    private float velocity;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-       
+        velocity = Input.GetAxisRaw("Horizontal");
+
+        // Flip sprite
+        if (velocity != 0)
+            sr.flipX = velocity > 0;
+
+        anim.SetFloat("Velocity", Mathf.Abs(velocity));
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
     }
+
     void FixedUpdate()
     {
-         velocity = Input.GetAxisRaw("Horizontal");
-        if (velocity != 0)
-            GetComponent<SpriteRenderer>().flipX = velocity > 0;
-        anim.SetFloat("Velocity", Mathf.Abs(velocity));
-        rb.linearVelocity = new Vector2(velocity*MoveSpeed*Time.deltaTime, 0);
+        rb.linearVelocity = new Vector2(velocity * MoveSpeed, rb.linearVelocity.y);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpForce); 
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draw ground check circle in editor
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
