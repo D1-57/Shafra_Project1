9768cbef494196c0bf9940812b43a9e117771bf7
tntksa1 <152206947+tntksa1@users.noreply.github.com>
2025-11-09 15:39:44 +0300

    Player Interaction

--- Assets/Script/PlayerInteraction.cs
+++ Assets/Script/PlayerInteraction.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    public int PlayerHealth;
+    public GameObject DeathSecren;
+    public GameObject WinSecten;
+    public GameObject PuaseScren;
+    public GameObject SettingScren;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
